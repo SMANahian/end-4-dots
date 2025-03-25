1aaf67a68fec07a51c0863b34a16acbc388fcf3e
SMANahian <smanahian123@gmail.com>
2025-03-26 05:18:15 +0600

    Fixing error of gammastep

--- modules/gammastep.nix
+++ modules/gammastep.nix
@@ -1,14 +1,13 @@
 { config, lib, pkgs, ... }:
 {
-  services.gammastep = lib.mkMerge [
-    {
+  config = {
+    services.gammastep = {
       enable = true;
       provider = "manual";
       latitude = 23.7;
       longitude = 90.35;
       temperature.day = 4500;
       temperature.night = 4500;
-    }
-    config.services.gammastep
-  ];
+    };
+  };
 }
